7c3515b00396b7b95bc9c1e899401a43f773fd89
julian molina <molina.bohemio@gmail.com>
2020-05-25 19:52:12 -0300

    add logs

--- app/interactors/games.js
+++ app/interactors/games.js
@@ -79,13 +79,20 @@ exports.playGame = async ({ gameId, x, y, flag }) => {
   let transaction = undefined;
   try {
     const game = await findGameWithCellsBy({ id: gameId });
+    logger.info('Game founded. Checking coordinates');
     if (game.width < x || game.height < y) throw invalidCoordinatesError();
+    logger.info('The coordinates are valid. Beginning the transaction');
     transaction = await sequelize.transaction();
+    logger.info('Checking if is a flag');
     const isValidFlag = flag || isNull(flag);
     if (isValidFlag) {
+      logger.info("It's a flag, updating the value in the cell");
       const { values, game: gameDataValues } = await updateFlag({ game, flag, x, y, transaction });
+      logger.info('Cell updated. Attempting to commit the changes');
       transaction.commit();
+      logger.info('The changes were committed. Checking if the game was win');
       const { won, values: finallyValues } = await checkAndUpdateGameWasWon(game);
+      logger.info(`¿Did the game win? : ${won}`);
       return {
         values: finallyValues || values,
         game: gameDataValues,
@@ -93,31 +100,46 @@ exports.playGame = async ({ gameId, x, y, flag }) => {
         won
       };
     }
+    logger.info("It isn't a flag. Discovering the cells");
     const { lost, cellIdsToUpdate, selectedCell, values } = await discoverCell({
       x: x - 1,
       y: y - 1,
       game,
       flag
     });
+    logger.info('The cells was discover correctly. Checking if the user lost the game');
     if (lost) {
+      logger.info('The user lost the game. Updating the game');
       await updateGame({ options: { where: { id: game.id }, transaction }, data: { state: FINISHED } });
+      logger.info('The game was updated successfully.Obtaining the finally values for the cells');
       const cellValuesWithMines = getLostCellValues(values);
+      logger.info('Cell values were obtained successfully. Commiting the changes');
       transaction.commit();
+      logger.info('The changes were committed successfully');
       return {
         values: cellValuesWithMines,
         lost,
         game: game.dataValues
       };
     }
+    logger.info("The user didn't lost.Obtaining the cell values");
     const cellValues = getNormalCellValues(values);
+    logger.info('The cell values were obtained successfully. Checking if exist only one cell to update');
     if (!cellIdsToUpdate) {
+      logger.info(
+        "It's a only cell to update. Checking if the cell already have a flag or if the user want to rollback a flag"
+      );
       const valueToUpdate = isValidFlag ? { value: flag } : { value: selectedCell.value };
+      logger.info('Updating the cell');
       await updateCell({
         data: { visible: true, ...valueToUpdate },
         options: { where: { id: selectedCell.id }, transaction }
       });
+      logger.info('Attempting to commit the changes');
       transaction.commit();
+      logger.info('The changes were committed. Checking if the user won the game');
       const { won, values: finallyValues } = await checkAndUpdateGameWasWon(game);
+      logger.info(`¿Did the game win? : ${won}`);
       const isOrContainFlag = isValidFlag || selectedCell.value;
       return {
         values:
@@ -129,11 +151,16 @@ exports.playGame = async ({ gameId, x, y, flag }) => {
         won
       };
     }
+    logger.info('Attempting to update many cells');
     await bulkUpdateCells({ cellIdsToUpdate, selectedCell, flag, transaction });
+    logger.info('Attempting to commit the changes');
     transaction.commit();
+    logger.info('The changes were committed successfuly');
     const { won, values: finallyValues } = await checkAndUpdateGameWasWon(game);
+    logger.info(`¿Did the game win? : ${won}`);
     return { values: finallyValues || cellValues, lost, game: game.dataValues, won };
   } catch (error) {
+    logger.error('There was an error playing the game', error);
     if (transaction) transaction.rollback();
     if (error.internalCode) throw error;
     throw internalServerError(error.message);
